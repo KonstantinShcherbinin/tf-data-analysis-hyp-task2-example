782e17950409c54ed6a516c3b6bd799ebb59f33d
k.scherbinin <k.scherbinin@digital-core.ru>
2023-04-13 16:47:20 +0500

    синтаксические ошибки

--- solution.py
+++ solution.py
@@ -11,7 +11,7 @@ def solution(x: np.array, y: np.array) -> bool:
     # Не меняйте название функции и её аргументы
 
     #res = stats.anderson_ksamp([x, y])
-    res = stats.cramervonmises_2samp([x, y])
+    res = stats.cramervonmises_2samp(x, y)
     #return res.statistic, res.critical_values, res.pvalue
     #return res.significance_level
     #return pval
