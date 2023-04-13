e589ff222db23cc4262630a1a572b69c4138a229
k.scherbinin <k.scherbinin@digital-core.ru>
2023-04-13 17:38:12 +0500

    10%

--- solution.py
+++ solution.py
@@ -10,13 +10,13 @@ def solution(x: np.array, y: np.array) -> bool:
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
 
-    #res = stats.anderson_ksamp([x, y])
-    res = stats.cramervonmises_2samp(x, y)
+    res = stats.anderson_ksamp([x, y])
+    #res = stats.cramervonmises_2samp(x, y)
     #return res.statistic, res.critical_values, res.pvalue
     #return res.significance_level
     #return pval
-    #if res.significance_level < 0.06:
-    if res.pvalue < 0.06:
+    if res.significance_level < 0.1:
+    #if res.pvalue < 0.06:
         return True
     else:
         return False
