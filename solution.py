39403d0a87bec27dace00b57f63ec50f134d88dc
k.scherbinin <k.scherbinin@digital-core.ru>
2023-04-13 15:54:55 +0500

    first_ct

--- solution.py
+++ solution.py
@@ -1,11 +1,20 @@
 import pandas as pd
 import numpy as np
+from scipy import stats
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 324151080 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+
+    res = stats.anderson_ksamp([x, y])
+    #return res.statistic, res.critical_values, res.pvalue
+    #return res.significance_level
+    #return pval
+    if res.significance_level < 0.06:
+        return True
+    else:
+        return False
